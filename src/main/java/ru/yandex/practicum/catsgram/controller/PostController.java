praktikum-java/module2_catsgram

add meet-spring_spring-boot-and-rest

src/main/java/ru/yandex/practicum/catsgram/controllers/PostController.java
@@ -1,0 +1,25 @@
+package ru.yandex.practicum.catsgram.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.catsgram.models.Post;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PostController {
+
+    private final List<Post> posts = new ArrayList<>();
+
+    @GetMapping("/posts")
+    public List<Post> findAll() {
+        return posts;
+    }
+
+    @PostMapping(value = "/post")
+    public void create(@RequestBody Post post) {
+        posts.add(post);
+    }
+}

src/main/java/ru/yandex/practicum/catsgram/controllers/SimpleController.java
@@ -1,0 +1,13 @@
+package ru.yandex.practicum.catsgram.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "Котограм";
+    }
+}

src/main/java/ru/yandex/practicum/catsgram/models/Post.java
@@ -1,0 +1,41 @@
+package ru.yandex.practicum.catsgram.models;
+
+import java.time.Instant;
+
+public class Post {
+
+    private final String author; // автор
+    private final Instant creationDate = Instant.now(); // дата создания
+    private String description; // описание
+    private String photoUrl; // url-адрес фотографии
+
+    public Post(String author, String description, String photoUrl) {
+        this.author = author;
+        this.description = description;
+        this.photoUrl = photoUrl;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}

Jariodson/catsgram

src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,0 +1,25 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.catsgram.model.Post;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PostController {
+
+    private final List<Post> posts = new ArrayList<>();
+
+    @GetMapping("/posts")
+    public List<Post> findAll() {
+        return posts;
+    }
+
+    @PostMapping(value = "/post")
+    public void create(@RequestBody Post post) {
+        posts.add(post);
+    }
+}

src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,0 +1,13 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "Котограм";
+    }
+}

src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -1,0 +1,41 @@
+package ru.yandex.practicum.catsgram.model;
+
+import java.time.Instant;
+
+public class Post {
+
+    private final String author; // автор
+    private final Instant creationDate = Instant.now(); // дата создания
+    private String description; // описание
+    private String photoUrl; // url-адрес фотографии
+
+    public Post(String author, String description, String photoUrl) {
+        this.author = author;
+        this.description = description;
+        this.photoUrl = photoUrl;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}